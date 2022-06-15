e4ea8161f11e61dc37feded29f1742700677b593
Peter Kmet <petekmet@gmail.com>
2022-06-15 15:39:44 +0200

    fix: too verbose logging removed

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -7,11 +7,10 @@ export async function initDb() {
     if(!env.GOOGLE_SERVICE_ACCOUNT){
         env.GOOGLE_SERVICE_ACCOUNT = "./datastore-service-account.json";
     }
-    console.log("env.GOOGLE_SERVICE_ACCOUNT:", process.env.GOOGLE_SERVICE_ACCOUNT);
+    console.log("env.GOOGLE_SERVICE_ACCOUNT:", env.GOOGLE_SERVICE_ACCOUNT);
 }
 
 export async function initMongoose() {
-    console.log("initMongoose:", process.env.MONGO_DB_URI);
     await mongoose.connect(env.MONGO_DB_URI!, { dbName: env.MONGO_DB_NAME });
     console.log("mongodb connected");
     // const NbiotDeviceModel = getModelForClass(NbiotDevice);
