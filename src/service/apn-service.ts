2850090396867f1058de1713a88333725062ed0f
Peter Kmet <petekmet@gmail.com>
2022-06-15 09:04:48 +0200

    feature: full uplink implementation

--- src/service/apn-service.ts
+++ src/service/apn-service.ts
@@ -3,11 +3,9 @@ import { AesCmac } from "aes-cmac";
 import { AddressInfo } from "net";
 import { Device } from "../model/device";
 import { createConnection } from "ts-datastore-orm";
-import { downlinkPacketHeader, unixtime, UplinkPacket, uplinkPacket, UplinkPacketHeader, uplinkPacketHeader } from "../utils/structbuffer";
-
-// sends response messages to the device
-
-// const key = Buffer.from("CB4E3EA400309DAB656D8DBFE4B93F35", "hex");
+import { UplinkPacket, uplinkPacket, UplinkPacketHeader, uplinkPacketHeader } from "../utils/structbuffer";
+import { messageWithMac } from "../utils/message-utils";
+import { createDownlinkMessage } from "../utils/message-utils";
 
 // emits on new datagram msg
 export async function apnServiceGetResponseBuffer(msg: Buffer, info: AddressInfo): Promise<Buffer> {
@@ -19,7 +17,7 @@ export async function apnServiceGetResponseBuffer(msg: Buffer, info: AddressInfo
     // decode header
     // const packet = uplinkPacket.decode(msg, true);
     const packetHeader: UplinkPacketHeader = uplinkPacketHeader.decode(msg, true);
-    const nbiotComposedAddress: string = msg.subarray(0,16).toString("hex");
+    const nbiotComposedAddress: string = msg.subarray(0, 16).toString("hex");
     console.log("NB-IoT device composed address:", nbiotComposedAddress);
     console.log("Decoded packet header:", packetHeader);
     // get device properties
@@ -29,54 +27,41 @@ export async function apnServiceGetResponseBuffer(msg: Buffer, info: AddressInfo
     const key = Buffer.from(d!.nwkSKey, "hex");
     console.log("Device properties:", d);
     // verify cmac
-    const cmac = msg.subarray(msg.length-4); // Buffer.from("2cc95bb0", "hex");
+    const cmac = msg.subarray(msg.length - 4);
     const cmacNumber = cmac.readInt32LE();
     const aesCmac = new AesCmac(key);
-    const result = aesCmac.calculate(msg.subarray(0,msg.length-4));
-    const calculatedCmacNumber: number = result.subarray(0,4).readInt32LE();
+    const result = aesCmac.calculate(msg.subarray(0, msg.length - 4));
+    const calculatedCmacNumber: number = result.subarray(0, 4).readInt32LE();
+    const downlinkData = d?.downlinkData ? Buffer.from(d.downlinkData, "hex") : undefined;
 
-    if(calculatedCmacNumber == cmacNumber) {
+    if (calculatedCmacNumber == cmacNumber) {
         console.log("Cmac ok");
         const packetPayload = msg.subarray(17); // skip header at pos 17
-        return processUplinkData(key, packetHeader, packetPayload);
-    }else{
+        return processUplinkData(key, packetHeader, packetPayload, downlinkData);
+    } else {
         console.log("Cmac verification failed");
     }
     return Buffer.from("");
 }
 
-function processUplinkData(key: Buffer, packetHeader: UplinkPacketHeader, packetPayload: Buffer): Buffer{
-    if(packetHeader.packetType === 0){
+function processUplinkData(
+    key: Buffer,
+    packetHeader: UplinkPacketHeader,
+    packetPayload: Buffer,
+    downlinkData?: Buffer
+): Buffer {
+    if (packetHeader.packetType === 0) {
         const packet = uplinkPacket.decode(new Uint8Array(packetPayload), true);
         console.log("Decoded uplink packet type 0:\n", packet);
         saveMessage(packet);
         if (packet.flags.downlinkRequest) {
-            // create downlink message, return unix timestamp if no message in DB
-            // set packet type based on message first byte
-            const header = downlinkPacketHeader.encode({
-                deviceId: packetHeader.deviceId,
-                subscriberId: packetHeader.subscriberId,
-                packetType: 0,
-                packetLength: 4,
-            }, true);
-            const payload = unixtime.encode({ timestamp: Math.trunc(Date.now() / 1000) });
-            const message = Buffer.concat([Buffer.from(header.buffer), Buffer.from(payload.buffer)]);
-            return messageWithMac(message, key);
-    
-        } else {
-            console.log("No downlink requested");
+            return messageWithMac(createDownlinkMessage(packetHeader, downlinkData), key);
         }
     }
     return Buffer.from("");
 }
 
-function messageWithMac(message: Buffer, key: Buffer): Buffer{
-    const aesCmac = new AesCmac(key);
-    const cmac = aesCmac.calculate(message);
-    return Buffer.concat([message, cmac.subarray(0,4)]);
-}
-
-function saveMessage(packet: UplinkPacket){
-    console.log("Measurement on", new Date(packet.measurementTimestamp*1000));
+function saveMessage(packet: UplinkPacket) {
+    console.log("Measurement on", new Date(packet.measurementTimestamp * 1000));
     console.log("Storing message...");
 }
---
+++ src/utils/message-utils.ts
@@ -0,0 +1,29 @@
+import { AesCmac } from "aes-cmac";
+import { downlinkPacketHeader, unixtime, UplinkPacketHeader } from "./structbuffer";
+
+export function createDownlinkMessage(packetHeader: UplinkPacketHeader, downlinkData?: Buffer): Buffer {
+    // create downlink message, return unix timestamp if no message in DB
+    // set packet type based on message first byte
+    var packetType = 0; // default
+    var packetLength = 4; // timestamp data size
+    var payload: ArrayBuffer = unixtime.encode({ timestamp: Math.trunc(Date.now() / 1000) }).buffer;
+    if(downlinkData && downlinkData.length > 0) {
+        packetType = downlinkData.readUint8(0);
+        packetLength = downlinkData.length - 1;
+        payload = downlinkData.subarray(1);
+    }
+    const header = downlinkPacketHeader.encode({
+        deviceId: packetHeader.deviceId,
+        subscriberId: packetHeader.subscriberId,
+        packetType: packetType,
+        packetLength: packetLength,
+    }, true);
+    console.log("Downlink payload:", payload);
+    return Buffer.concat([Buffer.from(header.buffer), Buffer.from(payload)]);
+}
+
+export function messageWithMac(message: Buffer, key: Buffer): Buffer {
+    const aesCmac = new AesCmac(key);
+    const cmac = aesCmac.calculate(message);
+    return Buffer.concat([message, cmac.subarray(0, 4)]);
+}
