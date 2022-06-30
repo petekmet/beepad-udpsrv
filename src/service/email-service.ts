a5dccbdaa6e2f4a56b45e1ef6baaad5c730edbf0
Peter Kmet <petekmet@gmail.com>
2022-06-30 08:48:22 +0200

    fix: crash when first measurement of new device

--- src/service/email-service.ts
+++ src/service/email-service.ts
@@ -14,36 +14,38 @@ const sesV2Config: SESv2ClientConfig = {
 export function processEmailAlerts(device: Device, measurement: Measurement) {
     // check battery state
     // check battery state
-    if(measurement.battery > 3700 && device.batteryLow == true) {
+    if (measurement.battery > 3700 && device.batteryLow == true) {
         device.batteryLow = false;
         // send battery back to normal email
         console.log("Battery back to normal");
-    }else
-    if(measurement.battery < 3400 && device.batteryLow == false) {
-        device.batteryLow = true;
-        // send battery low email
-        console.log("Battery low");
-    }
+    } else
+        if (measurement.battery < 3400 && device.batteryLow == false) {
+            device.batteryLow = true;
+            // send battery low email
+            console.log("Battery low");
+        }
 
     // check weight state
-    const deltaWeight = measurement.weight - device.lastMeasurement.weight;
-    if(Math.abs(deltaWeight) > 1) {
-        measurement.alarm = true;
-        // send alarm email
-        sendWeightAlertEmail(device, deltaWeight);
+    if (device.lastMeasurement) {
+        const deltaWeight = measurement.weight - device.lastMeasurement.weight;
+        if (Math.abs(deltaWeight) > 1) {
+            measurement.alarm = true;
+            // send alarm email
+            sendWeightAlertEmail(device, deltaWeight);
+        }
     }
 }
 
 async function sendWeightAlertEmail(device: Device, weightDelta: number) {
     const client = new SESv2Client(sesV2Config);
     const templateData = {
-        devicename:device.name,
+        devicename: device.name,
         deviceid: device._id,
         weight: weightDelta
     };
     const params: SendEmailCommandInput = {
         Content: {
-            Template:{
+            Template: {
                 TemplateName: "SK_WeightAlertTemplate",
                 TemplateData: JSON.stringify(templateData)
             }
